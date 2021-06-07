eb612e87b8ae2cbf494117d4a6e7ec3ebe3c3ac0
guoyang <992044394@qq.com>
2021-06-07 18:27:00 +0800

    learn webpack

---
+++ app/addImage.js
@@ -0,0 +1,10 @@
+import '../styles/addImage.css'
+
+let smallImg = document.createElement('img')
+// 必须 require 进来
+smallImg.src = require('../images/img1.png')
+document.body.appendChild(smallImg)
+
+// let bigImg = document.createElement('img')
+// bigImg.src = require('../images/img2.jpg')
+// document.body.appendChild(bigImg)
---
+++ app/index.js
@@ -0,0 +1,6 @@
+// var sum = require('./sum')
+// console.log(sum(1, 2))
+
+import sum from './sum'
+import './addImage'
+console.log(sum(1, 2));
